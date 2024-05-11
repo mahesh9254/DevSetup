0635407b23e3c2c02bf5e9dea9f7a0ff275a5c63
mahesh9254 <mahesh9254@gmail.com>
2024-05-11 16:24:24 +0530

    'CICD'

---
+++ GariboCICD.ps1
@@ -0,0 +1,46 @@
+﻿
+[CmdletBinding()]
+
+ param(
+
+     [Parameter(Mandatory)]
+
+     [pscredential]$Credential,
+
+     [Parameter(Mandatory)]
+
+     [string]$FilePath,
+
+     [Parameter(Mandatory)]
+
+     [string]$FtpServerHost
+
+ ) 
+
+git clone https://Mahesh92549254@bitbucket.org/maheshtest9254/test1.git
+cd cd.
+git switch master
+dotnet restore
+dotnet build
+cd WMS.ServiceApp.Adani
+dotnet publish
+cd \bin\Debug\netcoreapp3.1\publish
+
+$logFile = 'test.log'
+
+$files = get-childitem -path "$path" -file -recurse -include *.config, appsettings.*
+
+($files).fullname >> $logFile
+
+$files | Remove-Item
+
+$webclient = New-Object System.Net.WebClient
+
+$ftpPassword = $Credential.GetNetworkCredential().Password
+
+$webclient.Credentials = New-Object System.Net.NetworkCredential($Credential.Username, $ftpPassword)
+
+$remoteFileName = $FilePath | Split-Path -Leaf
+$uri = New-Object System.Uri("ftp://$FtpServerHost/$remoteFileName")
+$webclient.UploadFile($uri, $FilePath)
+$webclient.Dispose()
