1267f0a6bae0bab4d51f0faf7a7f7dcc434ffb73
mahesh9254 <mahesh9254@gmail.com>
2024-05-15 20:33:58 +0530

    'changes'

--- GariboCICD.ps1
+++ GariboCICD.ps1
@@ -1,7 +1,9 @@
 ﻿
 [CmdletBinding()]
-
  param(
+     [Parameter(Mandatory)]
+
+     [string]$RepositoryURL,
 
      [Parameter(Mandatory)]
 
@@ -15,22 +17,18 @@
 
      [string]$FtpServerHost
 
- ) 
-
-git clone https://Mahesh92549254@bitbucket.org/maheshtest9254/test1.git
-cd cd.
+ )
+git clone $RepositoryURL
+cd test1
 git switch master
+cd WMS.ServiceApp.Adani
 dotnet restore
 dotnet build
-cd WMS.ServiceApp.Adani
 dotnet publish
-cd \bin\Debug\netcoreapp3.1\publish
+cd bin\Debug\netcoreapp3.1\publish
 
-$logFile = 'test.log'
 
 $files = get-childitem -path "$path" -file -recurse -include *.config, appsettings.*
-
-($files).fullname >> $logFile
 
 $files | Remove-Item
 
@@ -38,9 +36,35 @@ $webclient = New-Object System.Net.WebClient
 
 $ftpPassword = $Credential.GetNetworkCredential().Password
 
+Write-Host $Credential.Username
+Write-Host $ftpPassword
 $webclient.Credentials = New-Object System.Net.NetworkCredential($Credential.Username, $ftpPassword)
 
-$remoteFileName = $FilePath | Split-Path -Leaf
-$uri = New-Object System.Uri("ftp://$FtpServerHost/$remoteFileName")
-$webclient.UploadFile($uri, $FilePath)
+#$remoteFileName = $FilePath | Split-Path -Leaf
+$uri = New-Object System.Uri("ftp://$FtpServerHost/Testcd")
+
+$files = Get-ChildItem -path "$path"  | Where-Object{!$_.PSIsContainer}
+
+foreach ($file in $files)
+{
+    Write-Host "Uploading $file"
+    $webclient.UploadFile("$uri/$file", $file.FullName)
+} 
+<#
+$folders = $files | Where-Object{$_.PSIsContainer}
+
+foreach ($folder in $folders)
+{
+	Write-Host "Uploading $folders"
+	$files = Get-ChildItem -path "./$folders" 
+	$uri = New-Object System.Uri("ftp://$FtpServerHost/$folder")
+   foreach ($file in $files)
+   {
+    Write-Host "Uploading $file"
+    $webclient.UploadFile("$uri/$file", $file.FullName)
+   } 
+    
+}#>
+#$webclient.UploadFile($uri, $FilePath)
+
 $webclient.Dispose()
