347db235b1aa4559c75e175f138e56b2e4f55001
mahesh9254 <mahesh9254@gmail.com>
2024-05-19 20:50:59 +0530

    web hook changes

--- WebHook/Program.cs
+++ WebHook/Program.cs
@@ -1,10 +1,18 @@
+using System.Web.Http;
+using Microsoft.Extensions.Logging.ApplicationInsights;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Logging.ClearProviders();
+builder.Logging.AddApplicationInsights(
+        configureTelemetryConfiguration: (config) =>
+            config.ConnectionString = builder.Configuration.GetSection("ApplicationInsights").GetValue<string>("APPLICATIONINSIGHTS_CONNECTION_STRING"),
+            configureApplicationInsightsLoggerOptions: (options) => { }
+    );
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -14,15 +22,15 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+
 app.UseHttpsRedirection();
-
-
-
 app.MapGet("/apiCICD", () =>
 {
     
 })
 .WithOpenApi();
+HttpConfiguration httpConfiguration = new HttpConfiguration();
+httpConfiguration.InitializeReceiveBitbucketWebHooks();
 app.Run();
 
 
